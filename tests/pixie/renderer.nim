0c73753a45c43365be489239189a251664eb31e5
David Krause <enthus1ast@users.noreply.github.com>
2022-09-09 17:35:48 +0200

    Signed-off-by: David Krause <enthus1ast@users.noreply.github.com>

--- tests/pixie/renderer.nim
+++ tests/pixie/renderer.nim
@@ -15,12 +15,27 @@ proc render(dr: DrawedQRCode, pixelSize = 512, bgColor = rgb(255, 255, 255), fgC
       if dr.drawing[xx.uint8, yy.uint8]:
         let pos = vec2((xx * ss).float, (yy * ss).float)
         let re = rect(pos, wh)
-        echo re
         ctx.fillRect(rect(pos, wh))
+        # circles just for fun:
+        # ctx.fillCircle(Circle(pos: pos, radius: (ss.float / 1.5 ).float))
   return image
 
 when isMainModule:
   import os
-  let urlQR = newQR("hallöäд")
-  let img = urlQR.render()
-  img.writeFile(getAppDir() / "img.png")
+  block:
+    let urlQR = newQR("hallöäд")
+    block:
+      let img = urlQR.render()
+      img.writeFile(getAppDir() / "img.png")
+    block:
+      let img = urlQR.render(64)
+      img.writeFile(getAppDir() / "img2.png")
+
+  block:
+    let urlQR = newQR("https://nim-lang.org")
+    block:
+      let img = urlQR.render()
+      img.writeFile(getAppDir() / "nl.png")
+    block:
+      let img = urlQR.render(64)
+      img.writeFile(getAppDir() / "nl2.png")
