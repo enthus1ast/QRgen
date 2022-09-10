f404c8631a2f0e406dcb947658e98eb695d761f5
David Krause <enthus1ast@users.noreply.github.com>
2022-09-10 14:18:17 +0200

    Signed-off-by: David Krause <enthus1ast@users.noreply.github.com>

--- tests/pixie/renderer.nim
+++ tests/pixie/renderer.nim
@@ -8,7 +8,7 @@ import options
 
 
 proc render(dr: DrawedQRCode, pixelSize = 512, light: string | ColorRGB = rgb(255, 255, 255),
-    dark: string | ColorRGB = rgb(0,0,0), centerImage: Option[Image] = none[Image]()): Image =
+    dark: string | ColorRGB = rgb(0,0,0), centerImage: Option[Image] = none[Image](), centerImageBlendMode = NormalBlend): Image =
   let ss = (pixelSize / dr.drawing.size.int).float32 # <- `div` = no lines but smaller img,   `/` = lines but good size
   let wh = vec2(ss.float, ss.float)
   print(pixelSize, dr.drawing.size, ss, wh)
@@ -30,7 +30,7 @@ proc render(dr: DrawedQRCode, pixelSize = 512, light: string | ColorRGB = rgb(25
       ((pixelSize / 2) - (centerImage.get().width / 2).float),
       ((pixelSize / 2) - (centerImage.get().height / 2).float),
     )
-    image.draw(centerImage.get(), transform = translate(cpos))
+    image.draw(centerImage.get(), transform = translate(cpos), blendMode = centerImageBlendMode)
   return image
 
 when isMainModule:
@@ -64,3 +64,17 @@ when isMainModule:
     let centerImg = readImage(getAppDir() / ".." / "tests" / "pixie" / "QRgen_logo_small_white.png")
     let img = urlQR.render(centerImage = some(centerImg))
     img.writeFile(getAppDir() / "img_withImg2.png")
+
+  block:
+    let urlQR = newQR("https://nim-lang.org")
+    let centerImg = readImage(getAppDir() / ".." / "tests" / "pixie" / "nimBadger.png")
+    let img = urlQR.render(centerImage = some(centerImg))
+    img.writeFile(getAppDir() / "img_withImg3.png")
+
+  block:
+    let urlQR = newQR("https://nim-lang.org", ecLevel = qrECH)
+    let centerImg = readImage(getAppDir() / ".." / "tests" / "pixie" / "nimBadger.png")
+    for bm in BlendMode:
+      let img = urlQR.render(centerImage = some(centerImg), centerImageBlendMode = bm)
+      img.writeFile(getAppDir() / "img_withImg4_" & $bm & ".png")
+
